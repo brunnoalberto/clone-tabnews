ae07ed8a9f89cc52d1cf6759e9800ea8b937ca88
Brunno Alberto <brunnoalbertokr@gmail.com>
2026-06-16 18:56:28 -0300

    Add versão do database no retorno do `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,16 +2,17 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;")
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
   response.status(200).json({
-    updated_at: updatedAt
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+      },
+    },
   });
 }
 
 export default status;
-
-/* 
-1. Versão do Postgres;
-2. Conexões máximas;
-3. Conexões usadas;
-*/
--- tests/integration/api/v1/get.test.js
+++ tests/integration/api/v1/get.test.js
@@ -7,4 +7,16 @@ test("Get to /api/v1/status should return 200", async () => {
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
 });
+
+
+
+
+// {
+//   "updated_at": "2026-06-16T21:46:04.898Z",
+//   "database_version": "PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit",
+//   "max_connections": "100",
+//   "used_connections": "7"
+// }
