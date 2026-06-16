f466d75d289d3000443633002895a31c08b3cbd4
Brunno Alberto <brunnoalbertokr@gmail.com>
2026-06-16 17:20:14 -0300

    configurando retorno do status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,17 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt
+  });
 }
 
 export default status;
+
+/* 
+1. Versão do Postgres;
+2. Conexões máximas;
+3. Conexões usadas;
+*/
